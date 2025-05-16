f222f4dbdb0a87fbf6bc86ec5f302ac4c881aa65
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-16 17:40:51 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by A S on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
