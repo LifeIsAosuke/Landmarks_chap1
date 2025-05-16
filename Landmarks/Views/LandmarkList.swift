7221b5e964f9d9b849736fd9e31c63e67da52eb2
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-17 01:04:10 +0900

    ユーザー入力の処理まで終了

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -5,26 +5,43 @@
 //  Created by A S on 2025/05/17.
 //
 
-import Foundation
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+
+
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+
     var body: some View {
         NavigationSplitView {
-            
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
-            .navigationTitle(Text("Landmarks"))
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
         }
     }
 }
+
 
 #Preview {
     LandmarkList()
