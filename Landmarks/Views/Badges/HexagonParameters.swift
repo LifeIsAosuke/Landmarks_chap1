f408d93850ff02c7217901cc7f293ea7da0cc763
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-17 02:02:50 +0900

    パスの図形の描画まで完了

--- Landmarks/Views/Badges/HexagonParameters.swift
+++ Landmarks/Views/Badges/HexagonParameters.swift
@@ -13,10 +13,17 @@ struct HexagonParameters {
         let curve: CGPoint
         let control: CGPoint
     }
-    
+
+
     static let adjustment: CGFloat = 0.085
-    
+
+
     static let segments = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
         Segment(
             line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
             curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
@@ -25,11 +32,7 @@ struct HexagonParameters {
         Segment(
             line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
             curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
-            control: CGPoint(x: 0.00, y: 0.75 - adjustment)        ),
-        Segment(
-            line:    CGPoint(x: 0.00, y: 0.70),
-            curve:   CGPoint(x: 0.05, y: 0.80),
-            control: CGPoint(x: 0.00, y: 0.75)
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
         ),
         Segment(
             line:    CGPoint(x: 0.40, y: 0.95),
