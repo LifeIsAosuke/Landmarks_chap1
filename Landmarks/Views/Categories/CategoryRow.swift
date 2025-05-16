164966891058ee64b740d2ea2ea33800be8e282a
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-17 05:21:37 +0900

    複雑なインターフェースを構成する終了

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -7,25 +7,32 @@
 
 import SwiftUI
 
+
 struct CategoryRow: View {
-    
     var categoryName: String
     var items: [Landmark]
-    
+
+
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading) {
             Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
                 .padding(.top, 5)
-            
-            ScrollView(.horizontal, showsIndicators: false){
+
+
+            ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }
+            .frame(height: 185)
         }
     }
 }
